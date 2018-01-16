02415cb5540e7ea5af59e8f1055c83e45a3a87fc
Ivan Andonov <ivan@andonov.info>
2018-01-16 14:22:46 +0200

    Add sortable option

---
+++ src/Traits/SortableTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Despark\Cms\Traits;
+
+use Rutorika\Sortable\SortableTrait as Sortable;
+
+/**
+ * Sortable trait
+ * extends Rutorika\Sortable\SortableTrait
+ */
+trait SortableTrait
+{
+    use Sortable;
+}
