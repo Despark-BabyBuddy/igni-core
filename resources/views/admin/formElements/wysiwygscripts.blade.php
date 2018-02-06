bd814e281d1bede7f046a170df6f1418d8e998d9
Ivan Andonov <ivan@andonov.info>
2018-02-06 12:18:20 +0200

    187-cms-rich-text-editor-formatting

--- resources/views/admin/formElements/wysiwygscripts.blade.php
+++ resources/views/admin/formElements/wysiwygscripts.blade.php
@@ -28,27 +28,56 @@
         external_plugins: {
             "filemanager": "{{ asset('/plugins/filemanager/plugin.min.js') }}"
         },
+        paste_as_text: true,
         media_live_embeds: true,
         style_formats: [
-            {title: 'Paragraph (p)', block: 'p', classes: ''},
-            {
-                title: 'Containers', items: [
-                {
-                    title: 'First div',
-                    block: 'div',
-                    classes: 'col-lg-4 col-lg-offset-2',
-                    wrapper: true,
-                    merge_siblings: false
-                },
-                {
-                    title: 'Second div',
-                    block: 'div',
-                    classes: 'col-lg-4',
-                    wrapper: true,
-                    merge_siblings: false
-                },
-            ]
-            }
+          {title: 'Headers', items: [
+            {title: 'Header 1', format: 'h1'},
+            {title: 'Header 2', format: 'h2'},
+            {title: 'Header 3', format: 'h3'},
+            {title: 'Header 4', format: 'h4'},
+            {title: 'Header 5', format: 'h5'},
+            {title: 'Header 6', format: 'h6'}
+          ]},
+          {title: 'Inline', items: [
+            {title: 'Bold', icon: 'bold', format: 'bold'},
+            {title: 'Italic', icon: 'italic', format: 'italic'},
+            {title: 'Underline', icon: 'underline', format: 'underline'},
+            {title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
+            {title: 'Superscript', icon: 'superscript', format: 'superscript'},
+            {title: 'Subscript', icon: 'subscript', format: 'subscript'},
+            {title: 'Code', icon: 'code', format: 'code'}
+          ]},
+          {title: 'Blocks', items: [
+            {title: 'Paragraph', format: 'p'},
+            {title: 'Blockquote', format: 'blockquote'},
+            {title: 'Div', format: 'div'},
+            {title: 'Pre', format: 'pre'}
+          ]},
+          {title: 'Alignment', items: [
+            {title: 'Left', icon: 'alignleft', format: 'alignleft'},
+            {title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
+            {title: 'Right', icon: 'alignright', format: 'alignright'},
+            {title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
+          ]},
+          {
+              title: 'Containers', items: [
+              {
+                  title: 'First div',
+                  block: 'div',
+                  classes: 'col-lg-4 col-lg-offset-2',
+                  wrapper: true,
+                  merge_siblings: false
+              },
+              {
+                  title: 'Second div',
+                  block: 'div',
+                  classes: 'col-lg-4',
+                  wrapper: true,
+                  merge_siblings: false
+              },
+              ]
+          }
         ],
         end_container_on_empty_block: true,
 
